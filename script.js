0325edaa54acf50c4b4a2915497b6b16127d0efc
dayounglee <verdantjuly@kakao.com>
2024-10-07 17:28:44 +0900

    [Feat] 랜덤 친구 찾기

---
+++ script.js
@@ -0,0 +1,49 @@
+// 개인 학습 : 랜덤 친구 찾기 페이지 만들기
+
+let beforeNumber = 0;
+
+const datas = [
+  {
+    image: "./profile2.jpg",
+    name: "조유리",
+    intro: "저는 백엔드 개발자입니다. 커피, 게임, 코딩을 좋아합니다.",
+    skills: [`Java`, `Node.js`, `Go`, `AWS`],
+  },
+  {
+    image: "./profile.png",
+    name: "차은우",
+    intro:
+      "저는 동적 웹 애플리케이션을 만드는 것을 좋아하는 풀스택 개발자입니다, 저는 독서, 하이킹, 전 세계 여행을 즐깁니다.",
+    skills: [`Javascript`, `HTML`, `CSS`],
+  },
+];
+
+function updateInfo(data) {
+  const { image, name, intro, skills } = data;
+  const imageElement = document.getElementById("profile-image");
+  const nameElement = document.getElementById("name");
+  const introElement = document.getElementById("intro");
+  const skillsBoxElement = document.getElementById("skills-box");
+  imageElement.src = image;
+  nameElement.textContent = name;
+  introElement.textContent = intro;
+  const skillsHTML = skills
+    .map((skill) => `<li class="small-box">${skill}</li>`)
+    .join("");
+  skillsBoxElement.innerHTML = skillsHTML;
+}
+
+function rotation() {
+  while (beforeNumber > -1) {
+    const randomNumber = Math.floor(Math.random() * datas.length);
+    if (beforeNumber !== randomNumber) {
+      updateInfo(datas[randomNumber]);
+      beforeNumber = randomNumber;
+      break;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateInfo(datas[beforeNumber]);
+});
